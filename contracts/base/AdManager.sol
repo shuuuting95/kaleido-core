736ffa0520f2d04916e294ea92f05c7c8ef5a2b5
shunp <shunpei42ba@gmail.com>
2021-08-14 18:49:10 +0900

    add sckelton

--- contracts/base/AdManager.sol
+++ contracts/base/AdManager.sol
@@ -8,6 +8,7 @@ import "../token/DistributionRight.sol";
 contract AdManager is NameAccessor {
 	struct PostContent {
 		uint256 postId;
+		address owner; // TODO:ERC721
 		string metadata;
 		uint256 currentPrice;
 		uint256 periodHours;
@@ -16,7 +17,20 @@ contract AdManager is NameAccessor {
 		address successfulBidder;
 	}
 
+	struct Bidder {
+		uint256 bidId;
+		uint256 postId;
+		address sender;
+		uint256 price;
+		string metadata;
+	}
+
+	// postId => PostContent
 	mapping(uint256 => PostContent) public allPosts;
+	// postId => Bidders
+	mapping(uint256 => Bidder[]) public allBidders;
+	// bidId => Bidder
+	mapping(uint256 => Bidder) public bidderInfo;
 
 	constructor(address nameRegistry) NameAccessor(nameRegistry) {}
 
@@ -26,13 +40,38 @@ contract AdManager is NameAccessor {
 		uint256 periodHours
 	) public {
 		PostContent memory post;
-		post.postId = IDGenerator.postId(metadata, block.number);
+		post.postId = IDGenerator.computePostId(metadata, block.number);
 		post.metadata = metadata;
 		post.currentPrice = currentPrice;
 		post.startTime = block.timestamp;
 		post.endTime = block.timestamp + periodHours;
 		allPosts[post.postId] = post;
-		_right().mint(msg.sender, post.postId);
+	}
+
+	function bid(uint256 postId, string memory metadata) public payable {
+		require(allPosts[postId].endTime > block.timestamp, "AD101");
+		Bidder memory bidder;
+		bidder.bidId = IDGenerator.computeBidId(postId, metadata);
+		bidder.postId = postId;
+		bidder.sender = msg.sender;
+		bidder.price = msg.value;
+		bidder.metadata = metadata;
+		allBidders[postId].push(bidder);
+	}
+
+	function close(uint256 bidId) public {
+		Bidder memory bidder = bidderInfo[bidId];
+		require(bidder.bidId != 0, "AD103");
+		require(allPosts[bidder.postId].owner == msg.sender, "AD102");
+
+		payable(msg.sender).transfer((bidder.price * 9) / 10);
+		payable(owner()).transfer((bidder.price * 1) / 10);
+		_right().mint(bidder.sender, bidId);
+	}
+
+	function refund(uint256 bidId) public {
+		require(bidderInfo[bidId].sender == msg.sender, "AD104");
+		payable(msg.sender).transfer(bidderInfo[bidId].price);
 	}
 
 	function _right() internal view returns (DistributionRight) {
--- contracts/libraries/IDGenerator.sol
+++ contracts/libraries/IDGenerator.sol
@@ -4,11 +4,19 @@ pragma solidity 0.8.6;
 library IDGenerator {
 	uint256 private constant ID_CAP = 10000000000000000;
 
-	function postId(string memory metadata, uint256 blockNumber)
+	function computePostId(string memory metadata, uint256 blockNumber)
 		public
 		pure
 		returns (uint256)
 	{
 		return uint256(keccak256(abi.encodePacked(blockNumber, metadata))) % ID_CAP;
+	}
+
+	function computeBidId(uint256 postId, string memory metadata)
+		public
+		pure
+		returns (uint256)
+	{
+		return uint256(keccak256(abi.encodePacked(postId, metadata))) % ID_CAP;
 	}
 }
