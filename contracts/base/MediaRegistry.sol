c675da385328f9fa60ea6167c6f69ed69fe77d7b
shunp <shunpei42ba@gmail.com>
2021-10-21 15:19:38 +0900

    add skelton for MediaRegistry

---
+++ contracts/base/MediaRegistry.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+import "./BlockTimestamp.sol";
+import "../accessors/NameAccessor.sol";
+import "hardhat/console.sol";
+
+/// @title MediaRegistry - registers a list of media accounts.
+/// @author Shumpei Koike - <shumpei.koike@bridges.inc>
+contract MediaRegistry is BlockTimestamp, NameAccessor {
+	mapping(address => address) public allAccounts;
+
+	function addMedia(
+		address proxy,
+		address owner /**onlyMediaFactory*/
+	) external {
+		allAccounts[proxy] = owner;
+	}
+
+	/// @dev Returns whether the account has created or not.
+	/// @param proxy address of the proxy contract that represents an account.
+	function created(address proxy) public view returns (bool) {
+		return allAccounts[proxy] != address(0x0);
+	}
+
+	/// @dev Returns the owner of the account.
+	/// @param proxy address of the proxy contract that represents an account.
+	function ownerOf(address proxy) public view returns (address) {
+		return allAccounts[proxy];
+	}
+}
