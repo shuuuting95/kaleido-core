eb4213f4f4f1a808da787279a4c2762c27436cf1
shunp <shunpei42ba@gmail.com>
2021-10-23 11:41:41 +0900

    enable to buy NFTs

---
+++ scripts/exec/buy.ts
@@ -0,0 +1,35 @@
+import { parseEther } from '@ethersproject/units'
+import { ethers } from 'ethers'
+import { getAdManagerABI } from '../common/file'
+import { getWallet } from '../common/wallet'
+import { gasLimit } from './../common/wallet'
+
+const network = process.env.NETWORK || 'ganache'
+
+const adminWallet = getWallet(0)
+
+const main = async () => {
+  /////////////////////////////////
+  // Param
+  const proxy = '0x987d858343237f0CD1A791ad5F4bC5DB0ae1085A'
+  const tokenId = 235237116874136
+  /////////////////////////////////
+
+  const manager = new ethers.Contract(proxy, getAdManagerABI(), adminWallet)
+
+  const tx = await manager.buy(tokenId, {
+    value: parseEther('0.1'),
+    gasLimit: gasLimit(),
+  })
+  const rc = await tx.wait()
+  const event = rc.events.find((event: any) => event.event === 'Buy')
+
+  console.log('event: ', event)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
